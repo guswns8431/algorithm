41f40a5892ce58d2291afb65a93f8aa9bbb0cdb0
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2020-07-02 23:38:52 +0900

    Create boj_2443.c

---
+++ boj_2443.c
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int maxStar = 0;
+    int beginStar = 0;
+    int currentStar = 0;
+    int countStar;
+    
+    cin >> n;
+    
+    maxStar = 2 * n - 1;
+    
+    for(int i = 1 ; i <= n ; i++)
+    {
+        currentStar = 2 * n - (2 * i - 1);
+        beginStar = (maxStar - currentStar) / 2 + 1;
+        countStar = 0;
+        for(int j = 1 ; j <= maxStar ; j++)
+        {
+            if(j >= beginStar)
+            {
+                cout << "*";
+                ++countStar;
+                
+            }
+            else
+            {
+                cout <<" ";
+            }
+            
+            if(currentStar == countStar)
+            {
+                break;
+            }
+        }
+        cout << "\n";
+    }
+    
+    return 0;
+}
