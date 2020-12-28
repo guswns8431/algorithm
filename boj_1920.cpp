5ab12aea1d09fe4cdc4ff41799cacf02b7d21230
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2020-12-28 16:17:38 +0900

    Update boj_1920.cpp

--- boj_1920.cpp
+++ boj_1920.cpp
@@ -1,4 +1,3 @@
-#include <ios>
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -7,26 +6,22 @@ int main(void)
 {
 	std::cin.tie(NULL);
 	std::ios_base::sync_with_stdio(false);
-	int a_num;
-	int m_num;
+	int num;
 	int tmp;
 	std::vector<int> a;
-	std::vector<int> m;
 
-	std::cin >> a_num;
-	for (int i = 0; i < a_num; i++)
+	std::cin >> num;
+	for (int i = 0; i < num; i++)
 	{
 		std::cin >> tmp;
 		a.push_back(tmp);
 	}
 	std::sort(a.begin(), a.end());
-	std::cin >> m_num;
-	for (int i = 0; i < m_num; i++)
+	std::cin >> num;
+	for (int i = 0; i < num; i++)
 	{
 		std::cin >> tmp;
-		m.push_back(tmp);
+		std::cout << binary_search(a.begin(), a.end(), tmp) << "\n";
 	}
-	for (int i = 0; i < m_num; i++)
-		std::cout << binary_search(a.begin(), a.end(), m[i]) << "\n";
 	return (0);
 }
