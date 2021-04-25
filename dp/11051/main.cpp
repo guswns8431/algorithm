0bb6c158e27233d6147ee7e47070e8fc8ad27f6d
guswns8431 <guswns8431@gmail.com>
2021-04-25 22:34:02 +0900

    11051

---
+++ dp/11051/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+int n, k;
+int dp[1001][1001];
+
+void input_setting()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+	std::cout.tie(0);
+}
+
+void input()
+{
+	std::cin >> n >> k;
+}
+
+void solution()
+{
+	for (int i = 0; i <= n; i++)
+		for (int j = 0; j <= k; j++)
+		{
+			if (i == j || j == 0)
+				dp[i][j] = 1;
+			else
+				dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % 10007;
+		}
+}
+
+void print()
+{
+	std::cout << dp[n][k];
+}
+
+int main(void)
+{
+	input_setting();
+	input();
+	solution();
+	print();
+	return (0);
+}
