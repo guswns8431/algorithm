8d60dc8fe09f4c49733306753717e28ee95d9fb8
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2023-09-07 21:30:55 +0900

    [Silver II] Title: 유기농 배추, Time: 4 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <queue>
+#include <utility>
+#include <algorithm>
+using namespace std;
+
+int m,n;
+int board[51][51];
+bool visited[51][51];
+
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {-1, 0, 1, 0};
+
+void bfs(int y, int x)
+{
+	queue<pair<int, int>> q;
+	int cur_x;
+	int cur_y;
+	int next_x;
+	int next_y;
+
+	visited[y][x] = true;
+
+	q.push({y, x});
+	while (!q.empty())
+	{
+		cur_x = q.front().second;
+		cur_y = q.front().first;
+		q.pop();
+		for (int i = 0; i < 4; i++)
+		{
+			next_x = cur_x + dx[i];
+			next_y = cur_y + dy[i];
+			if (next_x < 0 || next_x > m || next_y < 0 || next_y > n || board[next_y][next_x] == 0 || visited[next_y][next_x])
+				continue ;
+			visited[next_y][next_x] = true;
+			q.push({next_y, next_x});
+		}
+	}
+}
+
+int main(void)
+{
+	int t, k;
+	int x, y;
+	int answer;
+
+	cin >> t;
+	for (int i = 0; i < t; i++)
+	{
+		answer = 0;
+		fill(&board[0][0], &board[51][51], 0);
+		fill(&visited[0][0], &visited[51][51], 0);
+		cin >> m >> n >> k;
+		for (int j = 0; j < k; j++)
+		{
+			cin >> x >> y;
+			board[y][x] = 1;
+		}
+		for (int j = 0; j <= n; j++)
+		{
+			for (int l = 0; l <= m; l++)
+			{
+				if (board[j][l] == 1 && !visited[j][l])
+				{
+					bfs(j, l);
+					answer++;
+				}
+			}
+		}
+		cout << answer << "\n";
+	}
+	return (0);
+}
