cc97d43a9faa292eebd32487716cd783d4879db7
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2023-09-10 17:41:56 +0900

    [Silver II] Title: 예산, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int n, m;
+std::vector<int> input_arr;
+
+void input_setting()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+	std::cout.tie(0);
+}
+
+void input()
+{
+	std::cin >> n;
+	input_arr.resize(n);
+	for (int i = 0; i < n; i++)
+		std::cin >> input_arr[i];
+	std::cin >> m;
+}
+
+void solution()
+{
+	int start, mid, end;
+	int sum;
+
+	start = 0;
+	end = *std::max_element(input_arr.begin(), input_arr.end());
+	while (start <= end)
+	{
+		sum = 0;
+		mid = (start + end) / 2;
+		for (int i = 0; i < input_arr.size(); i++)
+		{
+			if (input_arr[i] - mid > 0)
+				sum += mid;
+			else
+				sum += input_arr[i];
+		}
+		if (sum <= m)
+			start = mid + 1;
+		else
+			end = mid - 1;
+	}
+	std::cout << end;
+}
+
+int main(void)
+{
+	input_setting();
+	input();
+	solution();
+	return (0);
+}
