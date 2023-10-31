5e60cfbd82a46ad21e9ffd3b16556e89c9cd607b
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2023-10-31 17:42:09 +0900

    [Bronze I] Title: 소인수분해, Time: 32 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void)
+{
+	int n;
+	vector<int> answer;
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+	if (n != 1)
+	{
+		while (n > 1)
+		{
+			for (int i = 2; i <= n; i++)
+			{
+				if (n % i == 0)
+				{
+					n /= i;
+					answer.push_back(i);
+					break ;
+				}
+			}
+		}
+		sort(answer.begin(), answer.end());
+		for (unsigned int i = 0; i < answer.size(); i++)
+			cout << answer[i] << "\n";
+	}
+	return (0);
+}
