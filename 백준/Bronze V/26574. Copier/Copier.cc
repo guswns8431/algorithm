250844e580d109812a0b05d297ee5ce4c179cffb
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2024-02-16 06:03:06 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze V/26574. Copier/Copier.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n; cin >> n;
+  for (int i = 0; i < n; i++) {
+    int num; cin >> num;
+
+    cout << num << " " << num << "\n";
+  }
+
+  return 0;
+}
