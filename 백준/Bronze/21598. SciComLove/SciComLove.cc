9c3a1d6eb5fdc362615fda3bb9b6833f9b31045c
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2024-02-20 19:15:43 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int n;
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cout << "SciComLove\n";
+	return (0);
+}
