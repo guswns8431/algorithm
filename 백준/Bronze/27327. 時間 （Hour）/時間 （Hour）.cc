9ccfdab6aff4d73ed6cc11650869dfc8467a1421
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2024-03-05 21:27:08 +0900

    [Bronze V] Title: 時間 (Hour), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int x;
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> x;
+
+	cout << x * 24;
+	return (0);
+}
