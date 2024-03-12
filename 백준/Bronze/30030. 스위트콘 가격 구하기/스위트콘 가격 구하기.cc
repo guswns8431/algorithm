b13fd9d5c21d2aa9b24511155bb96e421a19bbcd
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2024-03-12 20:52:45 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int b;
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> b;
+
+	cout << b * 100 / 110;
+	return (0);
+}
