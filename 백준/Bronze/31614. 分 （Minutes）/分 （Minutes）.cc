af6e8ddd3697ab7d63ba9fa2e4e5d6c3f8e170a3
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2024-03-17 13:32:21 +0900

    [Bronze V] Title: 分 (Minutes), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31614. 分 （Minutes）/分 （Minutes）.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int h, m;
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> h >> m;
+	cout << h * 60 + m;
+
+	return (0);
+}
