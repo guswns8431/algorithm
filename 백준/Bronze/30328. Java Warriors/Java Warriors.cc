768b6e0a036cff8b2624c46e2e96a88e920c7440
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2024-03-26 22:49:53 +0900

    [Bronze V] Title: Java Warriors, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int n;
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+    cin >> n;
+	cout << n * 4000;
+
+	return (0);
+}
