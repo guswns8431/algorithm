fb79b3a02b3149be54a3ff133d222af65633d527
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2024-04-26 11:02:54 +0900

    [Bronze III] Title: 경고, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3029. 경고/경고.py
+++ 백준/Bronze/3029. 경고/경고.py
@@ -6,14 +6,8 @@ sodium_time = int(input2[0]) * 3600 + int(input2[1]) * 60 + int(input2[2])
 
 answer_time = sodium_time - current_time
 
-if answer_time < 0:
+if answer_time <= 0:
     answer_time += 24 * 3600
-elif answer_time == 0:
-    answer_time_hour = 24
-    answer_time_minute = 0
-    answer_time_second = 0
-    print(f'{answer_time_hour:02d}:{answer_time_minute:02d}:{answer_time_second:02d}')
-    exit()
 answer_time_hour = answer_time // 3600
 answer_time %= 3600
 answer_time_minute = answer_time // 60
