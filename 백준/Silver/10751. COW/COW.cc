9d177c3e25a85e130e9cf6efe2a1a7755e88f404
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2024-08-10 16:41:07 +0900

    [Silver III] Title: COW, Time: 0 ms, Memory: 2392 KB -BaekjoonHub

---
+++ 백준/Silver/10751. COW/COW.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+long long c;
+long long co;
+long long cow;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin>>n;
+    string s;
+    cin>>s;
+    for(int i=0; i<s.size(); i++){
+        if(s[i]=='C')
+        c++;
+       else if(s[i]=='O')
+        co+=c;
+        else
+        cow+=co;
+    }
+    // cout<<"C: "<<c<<"\n";
+    // cout<<"O: "<<co<<"\n";
+    // cout<<"W: "<<cow<<"\n";
+
+    cout<<cow<<"\n";
+}
