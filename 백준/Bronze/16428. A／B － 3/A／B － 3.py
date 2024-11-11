f4fe4aadfabd6a41bd8ea828d3eec74682c91417
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2024-11-11 18:48:27 +0900

    [Bronze III] Title: A/B - 3, Time: 40 ms, Memory: 31252 KB, Score: 630 point -BaekjoonHub

--- 백준/Bronze/16428. A／B － 3/A／B － 3.py
+++ 백준/Bronze/16428. A／B － 3/A／B － 3.py
@@ -1,4 +1,27 @@
-a, b = map(int, input().split())
+import sys
 
-print(a // b)
-print(a % b)
+def main():
+    import sys
+    sys.setrecursionlimit(1 << 25)
+    A_str, B_str = sys.stdin.readline().split()
+    A = int(A_str)
+    B = int(B_str)
+    q, r = divmod_custom(A, B)
+    print(q)
+    print(r)
+
+def divmod_custom(A, B):
+    assert B != 0
+    q = A // B
+    r = A - q * B
+    if r < 0:
+        if B > 0:
+            q -= 1
+            r += B
+        else:
+            q += 1
+            r -= B
+    return q, r
+
+if __name__ == "__main__":
+    main()
