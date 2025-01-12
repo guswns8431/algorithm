8bee6765d262fa77986f4f40c998b236c4419182
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2025-01-12 23:55:48 +0900

    [Bronze IV] Title: I Speak TXTMSG, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/6841. I Speak TXTMSG/I Speak TXTMSG.py
@@ -0,0 +1,30 @@
+while True:
+    user_input = input()
+
+    if user_input == "CU":
+        print("see you")
+    elif user_input == ":-)":
+        print("I’m happy")
+    elif user_input == ":-(":
+        print("I’m unhappy")
+    elif user_input == ";-)":
+        print("wink")
+    elif user_input == ":-P":
+        print("stick out my tongue")
+    elif user_input == "(~.~)":
+        print("sleepy")
+    elif user_input == "TA":
+        print("totally awesome")
+    elif user_input == "CCC":
+        print("Canadian Computing Competition")
+    elif user_input == "CUZ":
+        print("because")
+    elif user_input == "TY":
+        print("thank-you")
+    elif user_input == "YW":
+        print("you’re welcome")
+    elif user_input == "TTYL":
+        print("talk to you later")
+        break
+    else:
+        print(user_input)
