ab7a66d17d789668a0e7ee1270523de6eedc5ed6
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2025-01-21 17:32:27 +0900

    [Bronze III] Title: 경기 결과, Time: 284 ms, Memory: 110840 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/5523. 경기 결과/경기 결과.py
+++ 백준/Bronze/5523. 경기 결과/경기 결과.py
@@ -1,9 +1,11 @@
-n = int(input())
+import sys
+
+n = int(sys.stdin.readline())
 
 a_score = 0
 b_score = 0
 for _ in range(n):
-    a, b = map(int, input().split())
+    a, b = map(int, sys.stdin.readline().split())
     if a > b:
         a_score += 1
     elif b > a:
