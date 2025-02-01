c9fe915ef8539e5faba7a969f789a08888c30f7f
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2025-02-01 16:01:26 +0900

    [Bronze I] Title: A/B - 2, Time: 68 ms, Memory: 38392 KB, Score: 2000 point -BaekjoonHub

--- 백준/Bronze/15792. A／B － 2/A／B － 2.py
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.py
@@ -1,4 +1,13 @@
 import sys
+import decimal
+
+# 소수점 정밀도 1100자리 설정 (여유 있게)
+decimal.getcontext().prec = 1100
 
 A, B = map(int, sys.stdin.readline().split())
-print(f"{A / B:.1000f}")
+
+# 정밀한 나눗셈 수행
+result = decimal.Decimal(A) / decimal.Decimal(B)
+
+# 소수점 1000자리까지 출력
+print(f"{result:.1000f}")
