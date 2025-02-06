09333b83f846b97ee627b7a25011b002b1d80cdf
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2025-02-06 11:45:13 +0900

    [Bronze II] Title: Poker Hand, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/16408. Poker Hand/Poker Hand.py
+++ 백준/Bronze/16408. Poker Hand/Poker Hand.py
@@ -1,11 +1,7 @@
-import operator
-
+user_input = input().split()
 rank = {}
-user_input = list(input().split())
 
-for i in range(0, len(user_input)):
-    rank.setdefault(user_input[i][0], 0)
-    rank[user_input[i][0]] += 1
-sorted_rank = sorted(rank.items(), key=operator.itemgetter(1), reverse=True)
-
-print(rank.get(sorted_rank[0][0], 0))
+for cards in user_input:
+    card = cards[0]
+    rank[card] = rank.get(card,0) + 1
+print(max(rank.values()))
