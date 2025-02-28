a4f90affc06af6d0d18d517354f9dcebc18305de
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2025-02-28 15:29:54 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.py
@@ -0,0 +1,11 @@
+for _ in range(15):
+    user_input = input()
+    if user_input.find('w') != -1:
+        print("chunbae")
+    elif user_input.find('b') != -1:
+        print("nabi")
+    elif user_input.find('g') != -1:
+        print("yeongcheol")
+    else:
+        continue
+    break
